b46fbb023d33436f2559fa80003dae9694c984d2
‘huangda2027’ <715745278@qq.com>
2022-03-31 11:25:02 +0800

    Update verify.js

--- Market9/js/verify.js
+++ Market9/js/verify.js
@@ -2,19 +2,22 @@ var nftId = localStorage.getItem("inforId");
 
 function verify() {
     var settings = {
-        "url": "http://localhost:8080/CertificateFunction/VerifyByNFTId?nftId="+nftId,
+        "url": "http://localhost:8080/CertificateFunction/VerifyByNFTId?nftId=" + nftId,
         "method": "post",
+        "xhrFields": {
+            withCredentials: true
+        },
         "timeout": 0,
-        };
-        //返回值
-        //response.result  1为验证成功，证书有效  -1为验证失败证书无效
-        $.ajax(settings).done(function (response) {
-            console.log(response);//
-            if(response.result == 1) {
-                alert("证书有效");
-            }
-            else if(response.result == -1) {
-                alert("证书无效");
-            }
-        });
+    };
+    //返回值
+    //response.result  1为验证成功，证书有效  -1为验证失败证书无效
+    $.ajax(settings).done(function (response) {
+        console.log(response);//
+        if (response.result == 1) {
+            alert("证书有效");
+        }
+        else if (response.result == -1) {
+            alert("证书无效");
+        }
+    });
 }
